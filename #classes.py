9401cc6d83de869ce06f54425b39f9f22bca934b
André Silva Mendes <andre.smendes.20231@poli.ufrj.br>
2023-10-03 12:39:35 -0300

    start

---
+++ #classes.py
@@ -0,0 +1,93 @@
+#classes
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://andre:GpGIBvmocawfazxa@cluster0.egthg3z.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+db = cluster["structure"]
+collection = db["users"]
+collection2 = db["alunos"]
+collection3 = db["tarefas"]
+collection4 = db["listas"]
+collection5 = db["notas"]
+
+#para usuarios   post = {"_id": 0, "name": "", "senha": "" }
+#para alunos     post = {"_id": 0, "name": ""}
+#para tarefas    post = {"_id": 0, "name": "", "descricao": "", "dataEntrega": "" , "entrega": ""}
+#para listas     post = {"_id": 0, "listaX": "", "descricao": "", "dataEntrega": "" , "entrega": ""}
+#para notas      post = {"_id": 0, "aluno_id": "", "tarefa_id": "", "nota": "" , "notaL": ""}
+
+class Usuario:
+    def __init__(self, _id, nome, senha):
+        self.___id =_id
+        self.nome = nome
+        self.senha = senha
+
+    def get_id(self):
+        return self.___id
+    
+    def autenticar(self, nome, senha):
+        pass    
+
+    def cadastrar():
+        #para usuarios   post = {"_id": 0, "name": "", "senha": "" }
+        pass
+
+class Aluno:
+    def __init__(self,_id,nome):
+        self.___id =_id
+        self.nome = nome
+    
+    def listar():
+        pass
+
+    def enviar():
+        pass
+
+    def calcularSituacao():
+        pass
+
+
+class Administrador:
+
+    def __init__(self, _id, nome):
+        self.___id =_id
+        self.nome = nome
+
+    def cadastrarTarefa():
+        pass
+
+    def calcularSituacao():
+        pass
+
+    def visualizarAlunos():
+        pass
+
+class Tarefa:
+
+    def __init__(self, _id, nome, descricao, dataEntrega):
+        self.___id =_id
+        self.nome = nome
+        self.descricao = descricao
+        self.dataEntrega = dataEntrega
+
+    def cadastrar():
+        pass
+
+    def listar():
+        pass
+
+    def receber():
+        pass
+
+class Notas:
+
+    def __init__(self, _id, aluno_id, tarefa_id, nota):
+        self.___id =_id
+        self.aluno_id = aluno_id
+        self.tarefa_id = tarefa_id
+        self.nota = nota
+
+    def cadastrar():
+        pass
+
+    def consultar():
+        pass
